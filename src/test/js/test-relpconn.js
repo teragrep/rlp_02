00ae7fcc908c220823f21b75afd5c34a0f2ed74d
spidermanja <38@teragrep.com>
2023-01-22 21:46:15 +0200

    Fix for the build

---
+++ src/test/js/test-relpconn.js
@@ -0,0 +1,47 @@
+/**
+ * This is an updated test case with Jasmine async works
+ * NOTE: Run the test case using Node might not work🚫
+ *  
+ */
+
+var config = require('dotenv');
+/*
+if(result.error) {
+    throw result.error
+}
+console.log(result.parsed);
+*/
+
+console.log(config.config());
+const RelpConnection = require("../../main/js/RelpConnection");
+const async = require('async');
+
+
+// Create the connection
+
+let relpConnection = new RelpConnection();
+let host = '127.0.0.1';
+let port = 1337; 
+let cfePort = 1601;
+let state = false;
+
+
+async function connect() {
+    state =  relpConnection.connect(cfePort, host);	
+    return state;
+}
+
+async function disconnect() {
+if(state){
+     relpConnection.disconnect();
+     console.log('Test for the flow....')
+}
+else {
+    console.log('Check the connection...')
+}	
+}
+
+beforeEach(async function() {
+    await connect();
+    await disconnect()
+})
